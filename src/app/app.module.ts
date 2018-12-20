a65a30f4710a81296c377b95d2e84e3e10de7921
Cathey191 <cathey191@hotmail.com>
2018-12-20 13:58:35 +1300

    Shopping list basic setup

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,25 +4,21 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
-import { RootComponent } from './root/root.component';
 import { HeaderComponent } from './header/header.component';
 import { RecipesComponent } from './recipes/recipes.component';
 import { RecipesListComponent } from './recipes/recipes-list/recipes-list.component';
-import { RecipesItemComponent } from './recipes/recipes-item/recipes-item.component';
 import { RecipesDetailComponent } from './recipes/recipes-detail/recipes-detail.component';
 import { ShoppingComponent } from './shopping/shopping.component';
 import { ShoppingListComponent } from './shopping/shopping-list/shopping-list.component';
-import { ShoppingEditComponent } from './shopping/shopping-edit/shopping-edit.component';
 import { RecipeItemComponent } from './recipes/recipes-list/recipe-item/recipe-item.component';
+import { ShoppingEditComponent } from './shopping/shopping-list/shopping-edit/shopping-edit.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    RootComponent,
     HeaderComponent,
     RecipesComponent,
     RecipesListComponent,
-    RecipesItemComponent,
     RecipesDetailComponent,
     ShoppingComponent,
     ShoppingListComponent,
